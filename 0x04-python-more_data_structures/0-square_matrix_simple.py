486b98555e0630b457dba4f2dba1eac4cd2d5da6
yunusa-usman <ibrahimyunusa245@gmail.com>
2023-01-13 08:13:34 +0000

    Tasks commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers of a matrix."""
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+ if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
